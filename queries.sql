e4915b51544ca795c4e2712dfa22906551a59a8e
maribelcuales <maribelcuales@gmail.com>
2020-09-03 05:41:33 +0800

    Added new file queries for SQL commands.

---
+++ queries.sql
@@ -0,0 +1,30 @@
+SELECT products.productName as product, products.Price, suppliers.SupplierName as SuppliedBy
+FROM products
+INNER JOIN Suppliers; 
+
+-- CategoryName, ProductName, Price 
+select c.CategoryName, p.ProductName, p.Price
+from categories as c
+join products as p
+where p.price >- 10
+order by p.ProductName;
+
+
+-- distinct products 
+select distinct (p.ProductName), p.Price
+from categories as c
+join products as p
+where p.price >- 10
+order by p.ProductName;
+
+-- grouping and counting (aggregates) 
+select categoryId, count(*)
+from products
+group by categoryId;
+
+-- calculate the average price per category 
+select categoryId, avg(price) as average
+from products
+group by categoryId
+order by average desc;
+
