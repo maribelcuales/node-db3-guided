ca6ab4dda617945c41490befc996c1067aaa4513
maribelcuales <maribelcuales@gmail.com>
2020-09-02 18:38:11 +0800

    Passed in findById function to add function

--- users/user-model.js
+++ users/user-model.js
@@ -13,13 +13,29 @@ function all() {
 } 
 
 function findById(id) {
-  return db("users").where({ id }).first();
+  return db("users").where("id", id).first(); 
 }
+// Other ways of writing .where(): 
+// .where({ id })
+// .where({ id: userId  })  => if userId was passed in as argument  
+// .where("id", id)
+// .where('id', '>', id)  
 
 function add(userData) {
   return db("users")
-  .insert(userData, "id");
+  .insert(userData, "id")
+  .then(ids => {
+    return findById(ids[0]); 
+  });
 } 
+
+/* 
+/// like a stack of plates  /// 
+db('users') => a promise that resolves to a user 
+findById => a promise that resolves to a user 
+add => a promise that resolves to a user 
+post 
+*/
 
 function update(id, changes) {
   return db("users")
